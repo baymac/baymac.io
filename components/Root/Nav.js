a57645f47056921ae314e10b4684493d3b176e55
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-06-27 12:41:49 +0530

    Move nav bar to top

--- components/Root/Nav.js
+++ components/Root/Nav.js
@@ -7,93 +7,89 @@ import {
   UilApps,
   UilMoon,
   UilSun,
-  UilPen
+  UilPen,
 } from "@iconscout/react-unicons";
 import rootStyles from "./root.module.css";
 import styles from "./nav.module.css";
 import cn from "classnames";
-import { useAppContext } from '../../context/AppContextProvider'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-
+import { useAppContext } from "../../context/AppContextProvider";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Nav() {
   const [navBarOpen, setNavBarOpen] = useState(false);
   const { darkMode, setDarkMode } = useAppContext();
 
-  const router = useRouter()
+  const router = useRouter();
 
-  return (
-    <header className={styles.header} id="header">
-      <nav className={cn(styles.nav, rootStyles.container)}>
-        {!router.pathname.startsWith('/posts') && <Link href="/" >
+  return (<div className={styles.header}>
+    <nav className={cn(styles.nav, rootStyles.container)}>
+      {!router.pathname.startsWith("/posts") && (
+        <Link href="/">
           <a className={styles.nav__logo}>PB</a>
-        </Link>}
-        {router.pathname.startsWith('/posts') && <Link href={`/blog`}>
-          <a className={styles.nav__logo}><UilArrowLeft /></a>
-        </Link>}
-        <div
-          className={cn(styles.nav__menu, {
-            [styles.nav__show_menu]: navBarOpen,
-          })}
-          id="nav-menu"
-        >
-          <ul className={cn(rootStyles.grid, styles.nav__list)}>
-            <li className="nav__item">
-              <Link href="/">
-                <a
-                  className={styles.nav__link}
-                >
-                  <UilHome className={styles.nav__icon} />
-                  Home
-                </a>
-              </Link>
-
-            </li>
-            <li>
-              <Link href="/about">
-                <a
-                  className={styles.nav__link}
-                >
-                  <UilMessage className={styles.nav__icon} />
-                  About
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <a
-                  className={styles.nav__link}
-                >
-                  <UilPen className={styles.nav__icon} />
-                  Blog
-                </a>
-              </Link>
-            </li>
-
-          </ul>
-          <UilTimes
-            className={styles.nav__close}
-            onClick={() => setNavBarOpen(false)}
-          />
-        </div>
-        <div className={styles.nav__btns}>
-          {createElement(
-            darkMode ? UilSun : UilMoon,
-            {
-              className: cn(styles.nav__changeTheme),
-              id: "theme-button",
-              onClick: () => setDarkMode(!darkMode)
-            },
-            null
-          )}
-          <UilApps
-            className={styles.nav__toggle}
-            id="nav_toggle"
-            onClick={() => setNavBarOpen(true)}
-          />
-        </div>
-      </nav>
-    </header>
+        </Link>
+      )}
+      {router.pathname.startsWith("/posts") && (
+        <Link href={`/blog`}>
+          <a className={styles.nav__logo}>
+            <UilArrowLeft />
+          </a>
+        </Link>
+      )}
+      <div
+        className={cn(styles.nav__menu, {
+          [styles.nav__show_menu]: navBarOpen,
+        })}
+        id="nav-menu"
+      >
+        <ul className={cn(rootStyles.grid, styles.nav__list)}>
+          <li className="nav__item">
+            <Link href="/">
+              <a className={styles.nav__link}>
+                <UilHome className={styles.nav__icon} />
+                Home
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className={styles.nav__link}>
+                <UilMessage className={styles.nav__icon} />
+                About
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a className={styles.nav__link}>
+                <UilPen className={styles.nav__icon} />
+                Blog
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <UilTimes
+          className={styles.nav__close}
+          onClick={() => setNavBarOpen(false)}
+        />
+      </div>
+      <div className={styles.nav__btns}>
+        {createElement(
+          darkMode ? UilSun : UilMoon,
+          {
+            className: cn(styles.nav__changeTheme),
+            id: "theme-button",
+            onClick: () => setDarkMode(!darkMode),
+          },
+          null
+        )}
+        <UilApps
+          className={styles.nav__toggle}
+          id="nav_toggle"
+          onClick={() => setNavBarOpen(true)}
+        />
+      </div>
+    </nav>
+  </div>
   );
 }
