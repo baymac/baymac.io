dc43c2e4c5b2e85158bc7b95de366b25d6c95d48
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-06-29 08:00:23 +0530

    Add styling to error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,10 +1,14 @@
+import styles from "../styles/error.module.css";
+
 function Error({ statusCode }) {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}
-    </p>
+    <div className={styles.container}>
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : "An error occurred on client"}
+      </p>
+    </div>
   );
 }
 
