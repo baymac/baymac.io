a4695291f8033ee7d302f4b828d0e06312b0d012
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-05 21:29:39 +0530

    Add nav bar items hover, selection, divider colors

--- components/Nav/NavbarLinks.tsx
+++ components/Nav/NavbarLinks.tsx
@@ -6,16 +6,39 @@ import {
 } from '@iconscout/react-unicons';
 import cn from 'classnames';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useAppContext } from '../../context/AppContextProvider';
 import styles from './navlink.module.css';
 
 export default function NavBarLinks() {
   const { setNavBarOpen } = useAppContext();
 
+  const router = useRouter();
+
+  const [selectedMenu, setSelectedMenu] = useState('home');
+
+  useEffect(() => {
+    if (router.asPath === '/') {
+      setSelectedMenu('home');
+    } else if (router.asPath === '/blog') {
+      setSelectedMenu('blog');
+    } else if (router.asPath === '/about') {
+      setSelectedMenu('about');
+    } else if (router.asPath === '/buymecrypto') {
+      setSelectedMenu('buymecrypto');
+    }
+  }, [router]);
+
   return (
     <ul className={cn(styles.nav__list)}>
       <Link href="/" passHref>
-        <li className={styles.nav__item} onClick={() => setNavBarOpen(false)}>
+        <li
+          className={cn(styles.nav__item, {
+            [styles.nav__item_selected]: selectedMenu === 'home'
+          })}
+          onClick={() => setNavBarOpen(false)}
+        >
           <a className={styles.nav__link}>
             <UilHome className={styles.nav__icon} />
             Home
@@ -23,7 +46,12 @@ export default function NavBarLinks() {
         </li>
       </Link>
       <Link href="/about" passHref>
-        <li className={styles.nav__item} onClick={() => setNavBarOpen(false)}>
+        <li
+          className={cn(styles.nav__item, {
+            [styles.nav__item_selected]: selectedMenu === 'about'
+          })}
+          onClick={() => setNavBarOpen(false)}
+        >
           <a className={styles.nav__link}>
             <UilMessage className={styles.nav__icon} />
             About
@@ -31,7 +59,12 @@ export default function NavBarLinks() {
         </li>
       </Link>
       <Link href="/blog" passHref>
-        <li className={styles.nav__item} onClick={() => setNavBarOpen(false)}>
+        <li
+          className={cn(styles.nav__item, {
+            [styles.nav__item_selected]: selectedMenu === 'blog'
+          })}
+          onClick={() => setNavBarOpen(false)}
+        >
           <a className={styles.nav__link}>
             <UilPen className={styles.nav__icon} />
             Blog
@@ -40,7 +73,9 @@ export default function NavBarLinks() {
       </Link>
       <Link href="/?buymecrypto=1" as="/buymecrypto" scroll={false} passHref>
         <li
-          className={cn(styles.nav__item, styles.hide_big_screen)}
+          className={cn(styles.nav__item, styles.hide_big_screen, {
+            [styles.nav__item_selected]: selectedMenu === 'buymecrypto'
+          })}
           onClick={() => setNavBarOpen(false)}
         >
           <a className={styles.nav__link}>
