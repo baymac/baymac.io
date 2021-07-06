92e7b7c43db97cdf7e9e49f58588f9c700cb843c
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-06 20:12:04 +0530

    Make useModalRouteRedirect return values react state instead of ref

--- hooks/useModalRouteRedirect.ts
+++ hooks/useModalRouteRedirect.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 function getOccurence(str: string, chr: string) {
   let count = 0;
@@ -29,18 +29,18 @@ export default function useModalRouteRedirect(
   const router = useRouter();
 
   // The redirect route when the modal closes
-  const redirectRoute = useRef('/');
+  const [redirectRoute, setRedirectRoute] = useState('/');
 
   // The actual route next router resolves e.g. posts/[id] or /?buymecrypto=1
-  const hrefRoute = useRef('/');
+  const [hrefRoute, setHrefRoute] = useState('/');
 
   // The route browser displays to user e.g. posts/ssg-ssr or /buymecrypto
-  const asRoute = useRef('/');
+  const [asRoute, setAsRoute] = useState('/');
 
   // Uncomment to debug
-  // console.log('redirect: ', redirectRoute.current);
-  // console.log('href: ', hrefRoute.current);
-  // console.log('as: ', asRoute.current);
+  // console.log('redirect: ', redirectRoute);
+  // console.log('href: ', hrefRoute);
+  // console.log('as: ', asRoute);
 
   useEffect(() => {
     // Check that modalRoutePath query param does not exist in path else we do not need to modify the redirect route
@@ -49,22 +49,22 @@ export default function useModalRouteRedirect(
       if (getOccurence(router.pathname, '/') > 1) {
         // This check is made for 2nd order paths as initially router doesn't contain query param in query, only contains the path param, so have to check it from asPath
         if (!pathContainsModalPathQuery(router.asPath, modalRoutePath)) {
-          hrefRoute.current = `${router.asPath}?${modalRoutePath}=1`;
-          asRoute.current = `${router.asPath}?${modalRoutePath}=1`;
-          redirectRoute.current = router.asPath;
+          setRedirectRoute(router.asPath);
+          setHrefRoute(`${router.asPath}?${modalRoutePath}=1`);
+          setAsRoute(`${router.asPath}?${modalRoutePath}=1`);
         } else {
           // Derive redirect path by removing query param
-          redirectRoute.current = removeQueryParamFromPath(router.asPath);
+          setRedirectRoute(removeQueryParamFromPath(router.asPath));
         }
       } else {
         // For any first order route (including root) we just add query param with current route and add dummy modalRoutePath route
-        redirectRoute.current = router.pathname;
-        hrefRoute.current = `${router.pathname}?${modalRoutePath}=1`;
-        asRoute.current = `/${modalRoutePath}`;
+        setRedirectRoute(router.pathname);
+        setHrefRoute(`${router.pathname}?${modalRoutePath}=1`);
+        setAsRoute(`/${modalRoutePath}`);
       }
     }
-  }, [modalRoutePath, router]);
+  }, [asRoute, hrefRoute, modalRoutePath, redirectRoute, router]);
 
   // Return values correct for navigating to a route modal in the UI, but returns default value for href and asRoute when modal route is loaded directly (by visiting the route or refreshing)
-  return [redirectRoute.current, hrefRoute.current, asRoute.current];
+  return [redirectRoute, hrefRoute, asRoute];
 }
