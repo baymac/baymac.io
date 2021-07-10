5e345edafbded7eca24c101de203c3d54ecc74ef
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-10 22:42:22 +0530

    Use focus-trap-react for modal focus

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import styles from './modal.module.css';
 import { UilTimesCircle } from '@iconscout/react-unicons';
 import usePreventScroll from '../../hooks/usePreventScroll';
+// import * as focusTrap from 'focus-trap'; // ESM
+import FocusTrap from 'focus-trap-react';
 
 export interface IModalProps {
   open: boolean;
@@ -29,24 +31,28 @@ function Modal(props: IModalProps) {
   const modalContent = open ? (
     <div role="presentation" className={styles.modal__overlay}>
       <div className={styles.modal__backdrop} aria-hidden="true"></div>
-      <div
-        className={styles.modal__container}
-        tabIndex={-1}
-        role="none presentation"
-      >
+      <FocusTrap>
         <div
-          className={styles.modal__content}
-          role="dialog"
-          aria-labelledby="simple-dialog-title"
+          className={styles.modal__container}
+          tabIndex={-1}
+          role="none presentation"
         >
-          <UilTimesCircle
-            onClick={(e) => handleCloseClick(e)}
-            className={styles.modal__close}
-            tabIndex={0}
-          />
-          {children}
+          {/* To prevent any content being focused on modal open */}
+          <div tabIndex={0}></div>
+          <div
+            className={styles.modal__content}
+            role="dialog"
+            id="modal-content"
+          >
+            <UilTimesCircle
+              onClick={(e) => handleCloseClick(e)}
+              className={styles.modal__close}
+              tabIndex={0}
+            />
+            {children}
+          </div>
         </div>
-      </div>
+      </FocusTrap>
     </div>
   ) : null;
 
