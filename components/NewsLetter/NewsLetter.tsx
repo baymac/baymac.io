6e77c090534b5341df792f50d5f5ad44121b5d49
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-11 22:15:52 +0530

    Add Basic Newsletter subscribe form

--- components/NewsLetter/NewsLetter.tsx
+++ components/NewsLetter/NewsLetter.tsx
@@ -5,10 +5,19 @@ export default function NewsLetter() {
   return (
     <>
       <div>
-        <h4 className={styles.header}>
-          Subscribe to the newsletter
-          <UilFastMail />
-        </h4>
+        <h3 className={styles.header}>
+          Subscribe to the newsletter&nbsp;
+          <UilFastMail size={30} />
+        </h3>
+        <div className={styles.input_box}>
+          <input type="text" id="name-input" placeholder="First Name" />
+        </div>
+        <div className={styles.input_box}>
+          <input type="email" id="email-input" placeholder="Email" />
+        </div>
+        <div className={styles.input_box}>
+          <input type="submit" value="Subscribe" />
+        </div>
       </div>
     </>
   );
