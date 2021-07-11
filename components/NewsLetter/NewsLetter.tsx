1f8a36399e788805ce5db678a89c905db4804598
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-11 22:41:12 +0530

    Add newsletter form and onSubmit

--- components/NewsLetter/NewsLetter.tsx
+++ components/NewsLetter/NewsLetter.tsx
@@ -1,7 +1,11 @@
 import styles from './newsletter.module.css';
 import { UilFastMail } from '@iconscout/react-unicons';
+import { useState } from 'react';
 
 export default function NewsLetter() {
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+
   return (
     <>
       <div>
@@ -9,15 +13,34 @@ export default function NewsLetter() {
           Subscribe to the newsletter&nbsp;
           <UilFastMail size={30} />
         </h3>
-        <div className={styles.input_box}>
-          <input type="text" id="name-input" placeholder="First Name" />
-        </div>
-        <div className={styles.input_box}>
-          <input type="email" id="email-input" placeholder="Email" />
-        </div>
-        <div className={styles.input_box}>
-          <input type="submit" value="Subscribe" />
-        </div>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            await fetch('/api/hello'); // demo
+          }}
+        >
+          <div className={styles.input_box}>
+            <input
+              type="text"
+              id="name-input"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className={styles.input_box}>
+            <input
+              type="email"
+              id="email-input"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.input_box}>
+            <input type="submit" value="Subscribe" />
+          </div>
+        </form>
       </div>
     </>
   );
