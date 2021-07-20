c8dce3c723de9b07eb99ae8625ac2600b74d3f16
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-20 23:26:44 +0530

    Attempt to fix path

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,7 +5,7 @@ const constants = {
   newsletterUnsubscribeApiRoute: '/api/newsletter/unsubscribe',
   newsletterUpdateApiRoute: '/api/newsletter/update',
   newsletterUpdateServerSideRoute: '/subscription/update',
-  verifyEmailTemplatePath: '/emailTemplates/emailconfirmation.html',
+  verifyEmailTemplatePath: '/public/emailTemplates/emailconfirmation.html',
 };
 
 export default constants;
