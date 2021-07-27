686699f407d1c632e0059254a01afaf30d23fa80
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-07-27 14:45:40 +0530

    Add sentry capture message to send email fn

--- lib/subscription/sendVerificationMail.ts
+++ lib/subscription/sendVerificationMail.ts
@@ -86,6 +86,7 @@ export default async function sendVerificationMail(
   };
   try {
     const result = await mailerClient.sendMail(mailOptions);
+    Sentry.captureMessage(`Mail sent to ${email}:${result.toString()}`);
     return {
       error: false,
       message: result.response,
