eda6276530baf3fbf79f32bb0d616161a04d3860
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-08-01 21:51:03 +0530

    Add unicode range and add prefetch rel to medium and roboto fonts (#105)
    * Add unicode range and add prefetch rel to medium and roboto fonts
    
    * Make inter medium preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,13 +13,6 @@ class MyDocument extends Document {
           {/* Fonts */}
           <link
             rel="preload"
-            href="/fonts/Inter-Medium.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
             href="/fonts/Inter-Regular.woff2"
             as="font"
             type="font/woff2"
@@ -27,6 +20,13 @@ class MyDocument extends Document {
           />
           <link
             rel="preload"
+            href="/fonts/Inter-Medium.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="prefetch"
             href="/fonts/Roboto-Light.woff2"
             as="font"
             type="font/woff2"
