ea7a894194b158804c0ddfeb446def27016c86ab
Parichay Barpanda <parichay.barpanda@gmail.com>
2021-10-18 23:24:02 +0530

    Improve next router mocking

--- tests/footer.test.js
+++ tests/footer.test.js
@@ -1,18 +1,35 @@
 /**
  * @jest-environment jsdom
  */
-
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import * as nextRouter from 'next/router';
+import { useRouter } from "next/router";
 
-nextRouter.useRouter = jest.fn();
-nextRouter.useRouter.mockImplementation(() => ({ pathname: '/' }));
+
+jest.mock("next/router", () => ({
+    useRouter: jest.fn(),
+}))
 
 import Footer from '../components/Footer/Footer'
 
 describe('Footer', () => {
+
+    const push = jest.fn();
+
+    useRouter.mockImplementation(() => ({
+        push,
+        pathname: "/",
+        route: "/",
+        asPath: "/",
+        query: "",
+    }));
+
     it('Should render home link', () => {
+        const mockRouter = {
+            push: jest.fn() // the component uses `router.push` only
+        };
+        (useRouter).mockReturnValue(mockRouter);
+
         const modalRoot = document.createElement('div');
         modalRoot.setAttribute('id', 'modal-root')
         document.body.appendChild(modalRoot)
